d577a653a0932747a492065addc7a02ac9dd355d
JUNIOR ALMEIDA FREITAS <082180035@ftt.cefsa.edu.br>
2022-03-16 16:24:18 -0300

    Commit aula 4

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+
+//Aula 1
+//alerta de popup com caixa de dialogo
+//alert('teste de alertas JS');
+//mesagem no console do navegador
+//console.log('teste de console');
+
+
+//Aula 3 - Variaveis 
+// var latitude = 40.87663;
+// var longitude = -8.0873;
+// console.log(latitude, longitude);
+
+//Aula 4
+// var nome = "João";
+// var sobrenome = "Gomes";
+// var cpf = '111.111.111-11';
+// var telefone = "970855254";
+// var ddd = "21";
+// var email = 'joao@mail.com';
+// var biografia = "João começou sua carreira como desenvolvedro web em 2010 e ja participou  de importantes projetos, como...";
+
+//  console.log("Nome Completo :" + nome + " " + sobrenome);
+//  console.log("(" + ddd + ")" + telefone);
+//  console.log ('Primeira letra do nome: ' + nome[0]);
+//  console.log ('Ultima letra do nome: ' + nome[3]);
+//  console.log ('Tamanho telefone: ' + telefone.length);
