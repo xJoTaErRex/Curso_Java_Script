1379d74b95e0bd4712800c7e0e59816e6af20c65
JUNIOR ALMEIDA FREITAS <082180035@ftt.cefsa.edu.br>
2022-03-16 17:03:22 -0300

    Commit aula 5

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,25 @@
 
 //Aula 1
+
 //alerta de popup com caixa de dialogo
 //alert('teste de alertas JS');
 //mesagem no console do navegador
 //console.log('teste de console');
 
+//----------------------------------------------------------------------------------------------
 
-//Aula 3 - Variaveis 
+
+//Aula 3 - Variaveis
+
 // var latitude = 40.87663;
 // var longitude = -8.0873;
 // console.log(latitude, longitude);
 
+//----------------------------------------------------------------------------------------------
+
+
 //Aula 4
+
 // var nome = "João";
 // var sobrenome = "Gomes";
 // var cpf = '111.111.111-11';
@@ -25,3 +33,48 @@
 //  console.log ('Primeira letra do nome: ' + nome[0]);
 //  console.log ('Ultima letra do nome: ' + nome[3]);
 //  console.log ('Tamanho telefone: ' + telefone.length);
+
+
+//----------------------------------------------------------------------------------------------
+
+// // Aula 5 - Numeros
+
+// var num1 = 20;
+// var num2 = 3;
+
+// var soma = num1 + num2;
+// var subtracao = num1 - num2;
+// var divisao = num1 / num2;
+// var multiplicacao = num1 * num2;
+
+// var media = soma/2;
+
+// console.log("Resultado da Media: " + media);
+
+// console.log(Math.pow(2,4));
+// console.log(Math.pow(num1,4));
+
+// var increment = 20;
+// // increment = increment + 5;
+// increment +=22;
+// console.log(increment);
+
+// increment -=5;
+// console.log(increment);
+
+
+// var telefone = "970855254";
+// var ddd = "21";
+
+// var ddd_string = ddd.toString();
+// var tel_string = telefone.toString();
+
+// console.log(ddd_string + tel_string);
+
+// idade = '17';
+
+// idade = parseInt(idade);
+// idade++;
+// console.log(idade);
+
+
