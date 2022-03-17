2328f43cb582a25a12f782c3504023c361894b16
JUNIOR ALMEIDA FREITAS <082180035@ftt.cefsa.edu.br>
2022-03-17 16:23:07 -0300

    Commit Aula 8 Exercicios (new)

---
+++ js/ex1.js
@@ -0,0 +1,29 @@
+//Aula 8 Ex
+
+//EX1 
+ console.log("EX1 - O Arquivo 'ex1.js' esta funcinando");
+
+//Ex 2
+
+var nome = "Junior";
+var sobrenome = "Almeida";
+var ano_nasc = '1998'
+var ano_nasc_int = parseInt(ano_nasc);
+console.log("EX2 - Olá, eu me chamo " + nome + " tenho " + (2022 - ano_nasc_int) + " anos e estou estudando Javascript")
+
+//Ex 3
+var nome = "Junior Almeida Freitas ";
+var numero_matricula = "8595666";
+var n1 = '8'
+var n2 = "7"
+var n1_int = parseInt(n1);
+var n2_int = parseInt(n2);
+console.log("EX3 - O aluno " + nome + ", matricula " + numero_matricula + ", obteve a média final "+ ((n1_int+n2_int)/2));
+
+//Ex 4
+var numero_telefone = "985956668";
+
+console.log("EX4 - Resultado do teste: " + (numero_telefone.length == 9));
+
+//Ex 4
+console.log(Math.pow(32,6));
